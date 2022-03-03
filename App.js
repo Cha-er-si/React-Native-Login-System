1af6af0a7c5410d1a2da2d205ca8cbf210e0d2c4
Prince Velarde <vpcharles31@gmail.com>
2022-03-03 14:35:12 +0800

    Signup Screen

--- App.js
+++ App.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 // Screens
 import Login from './screens/Login';
+import Signup from './screens/Signup';
 
 export default function App() {
-  return <Login />;
+  return <Signup />;
 }
